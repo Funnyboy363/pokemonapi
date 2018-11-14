7f4a52e6eb8dc697192b5c2d718b97edc591bcd6
Grant <grantshoop@gmail.com>
2018-11-13 21:53:39 -0700

    added pokemon images

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 import { pokemon } from './pokemon.js'
 
 console.log(pokemon)
+
+const pokeContainer = document.querySelector('#container')
+
+pokemon.forEach(poke => {
+    console.log(poke.ename)
+    let fig = document.createElement('figure')
+    let cap = document.createElement('figcaption')
+    cap.textContent = poke.ename
+    fig.appendChild(cap)
+    pokeContainer.appendChild(fig)
+})
