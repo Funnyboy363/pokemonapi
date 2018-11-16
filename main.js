c87ba3ceafbc413407d37b48b3c8dd748c25fb6b
Grant <grantshoop@gmail.com>
2018-11-15 22:07:18 -0700

    adjusted js so that the cards will show

--- main.js
+++ main.js
@@ -7,44 +7,9 @@ let count = 0
 
 
 
-// pokemon.forEach(poke => {
-//     count++
-//     console.log(`${poke.id}${poke.ename}`)
-//     let fig = document.createElement('figure')
-//     let img = document.createElement('img')
-//     let cap = document.createElement('figcaption')
-//     cap.textContent = poke.ename
-//     img.src = `images/${poke.id}${poke.ename}.png`
-//     pokeContainer.appendChild(fig)
-//     fig.appendChild(cap)
-//     fig.appendChild(img)
-    
-// })
-
-
-
-
-
-let sceneDiv = document.querySelector('.scene')
-
-const cardCreator = (pokemon) => {
-    let cardDiv = document.createElement('div')
-    cardDiv.addEventListener('click', function() {
-        cardDiv.classList.toggle('is-flipped');
-    })
-    let frontCard = document.createElement('div')
-    let backCard = document.createElement('div')
-
-    let name = document.createElement('p')
-    let height = document.createElement('p')
-    let mass = document.createElement('p')
-    name.textContent = `Name: ${pokemon.name}`
-    height.textContent = `Height: ${pokemon.height}`
-    mass.textContent = `Mass: ${pokemon.mass}`
-    backCard.appendChild(name)
-    backCard.appendChild(height)
-    backCard.appendChild(mass)
-    cardDiv.className = "card"
+pokemon.forEach(poke => {
+    count++
+    console.log(`${poke.id}${poke.ename}`)
     let fig = document.createElement('figure')
     let img = document.createElement('img')
     let cap = document.createElement('figcaption')
@@ -53,6 +18,41 @@ const cardCreator = (pokemon) => {
     pokeContainer.appendChild(fig)
     fig.appendChild(cap)
     fig.appendChild(img)
+    
+})
+
+
+
+
+
+// let sceneDiv = document.querySelector('.scene')
+
+// const cardCreator = (pokemon) => {
+//     let cardDiv = document.createElement('div')
+//     cardDiv.addEventListener('click', function() {
+//         cardDiv.classList.toggle('is-flipped');
+//     })
+//     let frontCard = document.createElement('div')
+//     let backCard = document.createElement('div')
+
+//     let name = document.createElement('p')
+//     let height = document.createElement('p')
+//     let mass = document.createElement('p')
+//     name.textContent = `Name: ${pokemon.name}`
+//     height.textContent = `Height: ${pokemon.height}`
+//     mass.textContent = `Mass: ${pokemon.mass}`
+//     backCard.appendChild(name)
+//     backCard.appendChild(height)
+//     backCard.appendChild(mass)
+//     cardDiv.className = "card"
+//     let fig = document.createElement('figure')
+//     let img = document.createElement('img')
+//     let cap = document.createElement('figcaption')
+//     cap.textContent = poke.ename
+//     img.src = `images/${poke.id}${poke.ename}.png`
+//     pokeContainer.appendChild(fig)
+//     fig.appendChild(cap)
+//     fig.appendChild(img)
     // frontCard.className = "card__face card__face--front"
     // backCard.className = "card__face card__face--back"
     // let frontImg = document.createElement('img')
@@ -64,23 +64,23 @@ const cardCreator = (pokemon) => {
   
 
     // now, add the front and back cards to the containing div
-    cardDiv.appendChild(frontCard)
-    cardDiv.appendChild(backCard)
-    sceneDiv.appendChild(cardDiv)
-    console.log(pokemon.name)
-};
+//     cardDiv.appendChild(frontCard)
+//     cardDiv.appendChild(backCard)
+//     sceneDiv.appendChild(cardDiv)
+//     console.log(pokemon.name)
+// };
 
-people.forEach(element => cardCreator(element))
+// people.forEach(element => cardCreator(element))
 
-let newCard = {
-    "name": "Grant Shoop",
-    "height": "71",
-    "mass": "70",
-    "birth_year": "1987",
-    "gender": "sir",
-}
+// let newCard = {
+//     "name": "Grant Shoop",
+//     "height": "71",
+//     "mass": "70",
+//     "birth_year": "1987",
+//     "gender": "sir",
+// }
 
-let createCard = document.querySelector('#createCard')
-createCard.addEventListener('click', () => {
-    return cardCreator(newCard)
-})
+// let createCard = document.querySelector('#createCard')
+// createCard.addEventListener('click', () => {
+//     return cardCreator(newCard)
+// })
